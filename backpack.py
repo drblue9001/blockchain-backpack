9a0c1b8e59eb1b2e4ab3aafe45dd165783bb3b57
Dr. Blue <drblue@blockchainbackpack.org>
2015-02-13 13:35:48 -0800

    First attempt at importing data.

--- backpack.py
+++ backpack.py
@@ -31,38 +31,49 @@ backpack_json = json.load(bp_file)['result']
 print "Loaded %d items from backpack." % len(backpack_json['items'])
 # TODO(drblue): Do more parsing on the schema file.
 
-print "Backpack item names: "
-for item in backpack_json['items']:
-  defindex = item['defindex']
-  schema_item = items_by_defindex[defindex];
-  schema_attributes_by_defindex = {}
-  if 'attributes' in schema_item:
-    for item_attr in schema_item['attributes']:
-      # Each attribute in the item definition doesn't list the defindex. We
-      # instead have to match by name
-      real_attr = attributes_by_name[item_attr['name']]
-      schema_attributes_by_defindex[real_attr['defindex']] = real_attr
-  print "  %s" % schema_item['item_name']
-  on_item_attr = {}
-  inherited_attr = {}
-  if 'attributes' in item:
-    for a in item['attributes']:
-      a_defindex = a['defindex']
-      if a_defindex in schema_attributes_by_defindex:
-        inherited_attr[a_defindex] = a
-      else:
-        on_item_attr[a_defindex] = a
-
-  if on_item_attr:
-    print "    On item attributes:"
-    for a in on_item_attr:
-      print "      %s " % attributes_by_defindex[a]['name']
-
-  if inherited_attr:
-    print "    Inheritted attributes:"
-    for a in inherited_attr:
-      print "      %s " % attributes_by_defindex[a]['name']
+# print "Backpack item names: "
+# for item in backpack_json['items']:
+#   defindex = item['defindex']
+#   schema_item = items_by_defindex[defindex];
+#   schema_attributes_by_defindex = {}
+#   if 'attributes' in schema_item:
+#     for item_attr in schema_item['attributes']:
+#       # Each attribute in the item definition doesn't list the defindex. We
+#       # instead have to match by name
+#       real_attr = attributes_by_name[item_attr['name']]
+#       schema_attributes_by_defindex[real_attr['defindex']] = real_attr
+#   print "  %s" % schema_item['item_name']
+#   on_item_attr = {}
+#   inherited_attr = {}
+#   if 'attributes' in item:
+#     for a in item['attributes']:
+#       a_defindex = a['defindex']
+#       if a_defindex in schema_attributes_by_defindex:
+#         inherited_attr[a_defindex] = a
+#       else:
+#         on_item_attr[a_defindex] = a
+#
+#   if on_item_attr:
+#     print "    On item attributes:"
+#     for a in on_item_attr:
+#       print "      %s " % attributes_by_defindex[a]['name']
+#
+#    if inherited_attr:
+#      print "    Inheritted attributes:"
+#      for a in inherited_attr:
+#        print "      %s " % attributes_by_defindex[a]['name']
 
 
 # If the filetime of the
-#print "Creating ethereum context..."
+print "Creating ethereum context..."
+s = tester.state()
+contract_file = open('contract.sol')
+c = s.abi_contract(contract_file.read(), language='solidity')
+c.CreateUser(65)
+for item in backpack_json['items']:
+  defindex = item['defindex']
+  schema_item = items_by_defindex[defindex];
+  print "Importing item %s..." % schema_item['item_name']
+  c.ImportItem(65, item['original_id'], defindex, item['level'],
+               item['quality'], item['origin'])
+
--- contract.sol
+++ contract.sol
@@ -1,75 +1,10 @@
-// - The problem is that there are properties which are short and repeating (worth using a symbol over) and there are properties which are long pieces of text (More than string32). Figure this out.
-// - In Valve's implementation, there are item properties which are strings.
-
-contract SymbolTable {
-  function SymbolTable() {
-    address sender = msg.sender;
-    owner = sender;
-    next_symbol = 0;
-  }
-
-  function DefineSymbol(string32 str) returns (uint16 symbol) {
-    address sender = msg.sender;
-    if (sender != owner || string_to_symbol[str] != 0) return;
-    next_symbol++;
-    symbol_to_string[next_symbol] = str;
-    string_to_symbol[str] = next_symbol;
-  }
-
-  function GetSymbolNumberFor(string32 str) constant returns (uint32 symbol) {
-    return string_to_symbol[str];
-  }
-
-  function GetStringForSymbol(uint16 symbol) constant returns (string32 str) {
-    return symbol_to_string[symbol];
-  }
-  
-  address owner;
-  uint32 next_symbol;
-  // TODO: All these things should be changed to arrays.
-  mapping (uint32 => string32) symbol_to_string;
-  mapping (string32 => uint32) string_to_symbol;
-}
-
-contract GameSchema {	
-  struct AttributeDefinition {
-  	  string32 name;
-  	  uint32 defindex;
-  	  uint16 attribute_class;  // is a symbol id
-    // TODO: Make this an array. Maps key names to values. This isn't really iterable.
-  	  mapping (uint32 => string32) values;
-  }
-
-  function GameSchema() {
-    address sender = msg.sender;
-    owner = sender;
-    table = new SymbolTable();
-  }
-
-  function SetAttributeDefinition(uint32 defindex, string32 name, uint16 attribute_class) {
-  	  AttributeDefinition a = attributes[defindex];
-  	  a.name = name;
-  	  a.defindex = defindex;
-  	  a.attribute_class = attribute_class;
-  }
-  
-  function SetAttributeProperty(uint32 defindex, uint32 key, string32 value) {
-  	  attributes[defindex].values[key] = value;
-  }
-    
-  address owner;
-  // TODO: Symbol table may collapse into two arrays.
-  SymbolTable table;
-  // TODO: This should be an array keyed on defindex.
-  mapping (uint32 => AttributeDefinition) attributes;
-  // TODO: 
-}
 
 // 
 contract BackpackSystem {
   // User 
   struct User {
     uint16 backpack_capacity;
+    uint16 num_items;
     // TODO: This should be an array of 18 * 100, the maximum
     // capacity of a backpack in the first place.
     mapping (uint16 => uint64) item_ids;
@@ -99,19 +34,60 @@ contract BackpackSystem {
   }
 
   // All items definitions are owned by the BackpackSystem.
-  mapping (int64 => ItemInstance) all_items;
+  address owner;
+  uint64 next_item_id;
+  mapping (uint64 => ItemInstance) all_items;
   mapping (address => User) user_backpacks;
 
-  // function CreateItem(uint32 defindex, 
+  function BackpackSystem() {
+    owner = msg.sender;
+    // As a way of having items both on chain, and off chain, compromise and
+    // say that the item ids are all off chain until item 4000000000, then all
+    // even numbers are on chain, and all odd numbers are off chain.
+    next_item_id = 4000000000;
+  }
 
+  function CreateUser(address user) {
+    if (msg.sender != owner) return;
+    User u = user_backpacks[user];
+    if (u.backpack_capacity == 0)
+      u.backpack_capacity = 300;
+  }
 
+  function AddBackpackSpaceForUser(address user) {
+    if (msg.sender != owner) return;
+    User u = user_backpacks[user];
+    if (u.backpack_capacity > 0 && u.backpack_capacity < 18000)
+      u.backpack_capacity += 100;
+  }
+
+  function ImportItem(address user, uint64 original_id, uint32 defindex,
+                      uint16 level, uint16 quality, uint16 origin)
+           returns (uint64 item_id) {
+    User u = user_backpacks[user];
+    if (u.backpack_capacity > 0 && u.num_items < u.backpack_capacity) {
+      // Create a new item using the next item id.
+      item_id = next_item_id;
+      next_item_id += 2;
+      ItemInstance i = all_items[item_id];
+      i.locked = false;
+      i.item_id = item_id;
+      i.original_id = original_id;
+      i.defindex = defindex;
+      i.level = level;
+      i.quality = quality;
+      i.quantity = 0;
+      i.origin = origin;
+
+      // Place a reference to this item in the users backpack.
+      u.item_ids[u.num_items] = item_id;
+      u.num_items++;
+    } else {
+      item_id = 0;
+    }
+  }
+  
   function LockItem(uint32 id) {
     
   }
 }
-
-// Interesting note: In the raw_tf2_bp.json, my items have the
-// attributes specified in the individual items. (ie, an instance of
-// "The Cozy Camper" has attribute 57 first, which is the defindex
-// of the health regen effect "health regen", which is the first
-// attribute in the schema.
